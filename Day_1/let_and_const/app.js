1ad2534e2297b884fa45282a6691d995a0f25d3a
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-01-29 18:33:40 +0530

    Add Day_1/let_and_const/app.js

---
+++ Day_1/let_and_const/app.js
@@ -0,0 +1,19 @@
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+
+  let r = readLine();
+
+  const PI = Math.PI;
+
+  // Print the area of the circle:
+
+  const area = PI * (r * r);
+
+  console.log(area);
+
+  // Print the perimeter of the circle:
+
+  const perimeter = 2 * (PI * r);
+
+  console.log(perimeter);
+}
