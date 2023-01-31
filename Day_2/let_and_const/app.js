7680e693592aa231d23a567f392b9cdc1b57807c
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-01-31 14:52:25 +0530

    Rename Day_1 and Day_2

---
+++ Day_2/let_and_const/app.js
@@ -0,0 +1,19 @@
+function calculate(r) {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+
+  const PI = Math.PI;
+
+  // Print the area of the circle:
+
+  const area = PI * (r * r);
+
+  console.log(area);
+
+  // Print the perimeter of the circle:
+
+  const perimeter = 2 * (PI * r);
+
+  console.log(perimeter);
+}
+
+calculate(2.6);
