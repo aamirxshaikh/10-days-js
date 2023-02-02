44931b5b97e25fd0c920789ebf7e099784484def
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-02-02 16:44:01 +0530

    Add Day_5/count_objects/app2.js - using filter

---
+++ Day_5/count_objects/app2.js
@@ -0,0 +1,24 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+
+// using filter
+
+function getCount(objects) {
+  return objects.filter((object) => {
+    return object.x === object.y;
+  }).length;
+}
+
+let objects = [
+  { x: 1, y: 1 },
+  { x: 2, y: 3 },
+  { x: 3, y: 3 },
+  { x: 3, y: 4 },
+  { x: 4, y: 5 },
+];
+
+console.log(getCount(objects));
